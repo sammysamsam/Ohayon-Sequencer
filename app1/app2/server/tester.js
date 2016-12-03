05b4fc3e617b855adcf44b434abb70a3431f4eba
Sam Mao <SAMMAO@Sams-Air.fios-router.home>
2016-12-03 14:28:31 -0500

    Major Update Ohayon App: Move updated version from private Seeman Lab Repository to local repository (first deleting old files)

--- app1/app2/server/tester.js
+++
@@ -1,5 +0,0 @@
-var java = require("java");
-java.classpath.push("./java");
-java.import('StackBuilder');
-var list = java.newInstanceSync("StackBuilder");
-console.log(list.RunAlgorithmSync("Na","1.0",["m,10,true,5,5,o-o-o-o-o-o-o-o-o-o-o","om,10,true,5,5,o-o-o-o-o-o-o-o-o-o-o"]));
